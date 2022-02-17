d587431c7d45d349426744bcbbe2ec8ece1fd34e
Alam <wwinda180@gmail.com>
2022-02-17 20:06:06 +0700

    Menambahkan tugas expressjs

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!<h1>');
 });
 
-// app.get('/about', (req, res) => {
-//   res.send('<h1>ini halaman about<h1>');
-// });
+app.get('/about', (req, res) => {
+  res.send('<h1>ini halaman about<h1>');
+});
 
-// app.get('/json', (req, res) => {
-//   res.json({
-//     nama: 'Ari Alamsyah',
-//     kelas: 'MERN Stack',
-//     instansi: 'Eduwork',
-//   });
-// });
+app.get('/json', (req, res) => {
+  res.json({
+    nama: 'Ari Alamsyah',
+    kelas: 'MERN Stack',
+    instansi: 'Eduwork',
+  });
+});
 
-// app.use('/', (req, res) => {
-//   res.status(404);
-//   res.send('<h1>Page not found 404<h1>');
-// });
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send('<h1>Page not found 404<h1>');
+});
 
 app.listen(process.env.PORT || 3000);
